682f7d75d958d53a1b4a104c3b00625b39174fd3
SpiderMath <71999854+SpiderMath@users.noreply.github.com>
2021-04-07 13:07:01 +0530

    Added image/kyon-gun :gun: :dancer:

--- Source/Routers/Image.ts
+++ Source/Routers/Image.ts
@@ -610,4 +610,43 @@ ImageRouter.get("/he-lives-in-you", async (req, res) => {
 		.send(canvas.toBuffer());
 });
 
+ImageRouter.get("/kyon-gun", async (req, res) => {
+	const imageURL = req.query.image;
+
+	if(!imageURL) {
+		return res
+			.status(400)
+			.json({
+				error: "Image URL not provided",
+			});
+	}
+
+	let image;
+
+	try {
+		// @ts-ignore
+		image = await loadImage(imageURL);
+	}
+	catch(err) {
+		return res
+			.status(400)
+			.json({
+				error: "Failed to load Image",
+			});
+	}
+
+	const base = await loadImage(join(__dirname, "../../Assets/Images/kyon-gun.png"));
+
+	const canvas = createCanvas(base.width, base.height);
+	const ctx = canvas.getContext("2d");
+
+	ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
+	ctx.drawImage(base, 0, 0, canvas.width, canvas.height);
+
+	res
+		.status(200)
+		.set({ "Content-Type": "image/png" })
+		.send(canvas.toBuffer());
+});
+
 export default ImageRouter;
