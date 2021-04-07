43fd1babb5961a90841d6884c7270c9bdc708965
SpiderMath <71999854+SpiderMath@users.noreply.github.com>
2021-04-07 23:33:52 +0530

    Added image/sip :tea: :coffee: :yum:
    Plan to Add flipped version as well :tm:

--- Source/Routers/Image.ts
+++ Source/Routers/Image.ts
@@ -649,4 +649,46 @@ ImageRouter.get("/kyon-gun", async (req, res) => {
 		.send(canvas.toBuffer());
 });
 
+ImageRouter.get("/sip", async (req, res) => {
+	const imageURL = req.query.image;
+
+	if(!imageURL) {
+		return res
+			.status(400)
+			.json({
+				error: "Image URL not provided",
+			});
+	}
+
+	let image;
+
+	try {
+		// @ts-ignore
+		image = await loadImage(imageURL);
+	}
+	catch(err) {
+		return res
+			.status(400)
+			.json({
+				error: "Failed to load Image",
+			});
+	}
+
+	const base = await loadImage(join(__dirname, "../../Assets/Images/sip.png"));
+
+	const canvas = createCanvas(base.width, base.height);
+	const ctx = canvas.getContext("2d");
+
+	ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+	ctx.drawImage(image, 0, 0, 512, 512);
+
+	ctx.drawImage(base, 0, 0, canvas.width, canvas.height);
+
+	res
+		.status(200)
+		.set({ "Content-Type": "image/png" })
+		.send(canvas.toBuffer());
+});
+
 export default ImageRouter;
