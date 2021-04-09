8cae5cda7f46490e27017e6e5b8ce9d37eb7fac5
SpiderMath <71999854+SpiderMath@users.noreply.github.com>
2021-04-09 14:13:21 +0530

    Added image/i-have-the-power :zap: Took 4 hours

--- Source/Routers/Image.ts
+++ Source/Routers/Image.ts
@@ -721,4 +721,48 @@ ImageRouter.get("/sip", async (req, res) => {
 		.send(canvas.toBuffer());
 });
 
+ImageRouter.get("/i-have-the-power", async (req, res) => {
+	const imageURL = req.query.image;
+
+	if(!imageURL) {
+		return res
+			.status(400)
+			.json({
+				error: "Image URL not provided",
+			});
+	}
+
+	let image;
+
+	try {
+		// @ts-ignore
+		image = await loadImage(imageURL);
+	}
+	catch(err) {
+		return res
+			.	status(400)
+			.json({
+				error: "Failed to load Image",
+			});
+	}
+
+	const base = await loadImage(join(__dirname, "../../Assets/Images/i-have-the-power.png"));
+
+	const canvas = createCanvas(base.width, base.height);
+	const ctx = canvas.getContext("2d");
+
+	ctx.drawImage(base, 0, 0, base.width, base.height);
+
+	ctx.rotate(18.3 * (Math.PI / 180));
+
+	ctx.drawImage(image, 332, -125, 175, 175);
+
+	ctx.rotate(-18.3 * (Math.PI / 180));
+
+	res
+		.status(200)
+		.set({ "Content-Type": "image/png" })
+		.send(canvas.toBuffer());
+});
+
 export default ImageRouter;
