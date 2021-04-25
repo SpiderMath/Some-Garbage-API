481a1181d86b9216b166400489fa07696d929c91
SpiderMath <71999854+SpiderMath@users.noreply.github.com>
2021-04-25 07:16:52 +0530

    Started Legend.ts

---
+++ Source/Routers/Image/Routes/Legend.ts
@@ -0,0 +1,14 @@
+import RouteExport from "../../../Constants/RouteExport";
+
+const LegendRoute: RouteExport = {
+	name: "legend",
+	description: "Makes the person a Legend!",
+	async run(req, res) {
+		try {}
+		catch(err) {
+			res.send("");
+		}
+	},
+};
+
+export default LegendRoute;
